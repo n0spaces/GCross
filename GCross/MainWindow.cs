aec1bb065d49d862eabd951a08f1538595fafb36
Matt Schwartz <7495188+n0spaces@users.noreply.github.com>
2021-01-30 14:04:49 -0600

    Add file dialog and improve clue readability

--- GCross/MainWindow.cs
+++ GCross/MainWindow.cs
@@ -6,6 +6,20 @@ namespace GCross
 {
     class MainWindow : Window
     {
+#pragma warning disable 649
+        // Ignore "never assigned" warnings. These are assigned in builder.Autoconnect() in the constructor.
+        [UI] private ModelButton _btnAbout;
+        [UI] private ModelButton _btnOpen;
+        
+        [UI] private FileChooserNative _fileChooserNative;
+
+        [UI] private AboutDialog _aboutDialog;
+        [UI] private ButtonBox _aboutButtonBox;
+        [UI] private Button _btnAboutClose;
+#pragma warning restore 649
+
+        private PuzzleDrawingArea puzzleArea;
+        
         public MainWindow() : this(new Builder("MainWindow.glade"))
         {
         }
@@ -13,17 +27,67 @@ namespace GCross
         private MainWindow(Builder builder) : base(builder.GetObject("MainWindow").Handle)
         {
             builder.Autoconnect(this);
+
+            _btnAboutClose = (Button) _aboutButtonBox.Children[2];
             
-            DrawingArea puzzleArea = new PuzzleDrawingArea(Puzzle.LoadFromCsv("pattern.csv"));
+            DeleteEvent += WindowOnDeleteEvent;
+            _btnOpen.Clicked += BtnOpenOnClicked;
+            _btnAbout.Clicked += BtnAboutOnClicked;
+            _aboutDialog.DeleteEvent += AboutDialogOnDeleteEvent;
+            _btnAboutClose.Clicked += BtnAboutCloseOnClicked;
+
+            puzzleArea = new PuzzleDrawingArea();
             Add(puzzleArea);
             ShowAll();
-
-            DeleteEvent += Window_DeleteEvent;
         }
 
-        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        private void LoadNewPuzzle(string filepath)
+        {
+            puzzleArea.Puzzle = Puzzle.LoadFromCsv(filepath);
+        }
+
+        private void BtnOpenOnClicked(object sender, EventArgs e)
+        {
+            /*
+            FileChooserDialog fc = new FileChooserDialog("Select the puzzle file to open", this,FileChooserAction.Open,
+                "Cancel", ResponseType.Cancel, "Open", ResponseType.Accept);
+
+            if (fc.Run() == (int) ResponseType.Accept)
+            {
+                LoadNewPuzzle(fc.Filename);
+            }
+            fc.Dispose();
+            */
+            if (_fileChooserNative.Run() == (int) ResponseType.Accept)
+            {
+                LoadNewPuzzle(_fileChooserNative.Filename);
+            }
+        }
+
+
+        private void BtnAboutOnClicked(object sender, EventArgs e)
+        {
+            _aboutDialog.Show();
+        }
+
+        private void WindowOnDeleteEvent(object sender, DeleteEventArgs a)
         {
             Application.Quit();
         }
+
+        #region About Dialog Events
+
+        private void BtnAboutCloseOnClicked(object sender, EventArgs e)
+        {
+            _aboutDialog.Visible = false;
+        }
+
+        private void AboutDialogOnDeleteEvent(object o, DeleteEventArgs args)
+        {
+            _aboutDialog.Visible = false;
+            args.RetVal = true;
+        }
+
+        #endregion
     }
 }
--- GCross/PuzzleDrawingArea.cs
+++ GCross/PuzzleDrawingArea.cs
@@ -26,7 +26,7 @@ namespace GCross
         private (int X, int Y) _mouse;
         private (int X, int Y) _lastMouse;
         private bool _mouseEntered;
-        private Puzzle _puzzle;
+        public Puzzle Puzzle;
 
         private CellStatus _clickAction;
 
@@ -47,7 +47,7 @@ namespace GCross
             EnterNotifyEvent += OnMouseEnter;
             LeaveNotifyEvent += OnMouseLeave;
 
-            _puzzle = puzzle;
+            Puzzle = puzzle;
         }
 
 #region Event methods
@@ -151,14 +151,14 @@ namespace GCross
         /// <param name="cr">Cairo context</param>
         private void DrawPuzzle(Context cr)
         {
-            if (_puzzle == null) return;
+            if (Puzzle == null) return;
             
             cr.Antialias = Antialias.Subpixel;
             cr.LineCap = LineCap.Square;
 
             _selectedIndex = GetHoveredCell();
             HandleClickedCells();
-            _puzzle.UpdateSolvedClues();
+            Puzzle.UpdateSolvedClues();
 
             ScaleToFit(cr);
             DrawBackground(cr);
@@ -176,11 +176,11 @@ namespace GCross
         private void ScaleToFit(Context cr)
         {
             // Determine the size of the puzzle, including clues
-            _cluesHeight = _puzzle.ColumnClues.Select(n => n.Length).Max() * CellSize;
-            _cluesWidth = _puzzle.RowClues.Select(n => n.Length).Max() * CellSize;
+            _cluesHeight = Puzzle.ColumnClues.Select(n => n.Length).Max() * CellSize;
+            _cluesWidth = Puzzle.RowClues.Select(n => n.Length).Max() * CellSize;
 
-            int drawWidth = _puzzle.Width * CellSize + PuzzleMargin * 2 + _cluesWidth;
-            int drawHeight = _puzzle.Height * CellSize + PuzzleMargin * 2 + _cluesHeight;
+            int drawWidth = Puzzle.Width * CellSize + PuzzleMargin * 2 + _cluesWidth;
+            int drawHeight = Puzzle.Height * CellSize + PuzzleMargin * 2 + _cluesHeight;
             
             // Scale and transform the puzzle as needed
             double sx = (double) AllocatedWidth / drawWidth;
@@ -212,10 +212,10 @@ namespace GCross
         private void DrawBackground(Context cr)
         {
             cr.SetSourceRGBA(1, 1, 1, 1);
-            cr.Rectangle(_puzzleStart.X, PuzzleMargin, _puzzle.Width * CellSize,
-                _puzzle.Height * CellSize + _cluesHeight);
-            cr.Rectangle(PuzzleMargin, _puzzleStart.Y, _puzzle.Width * CellSize + _cluesWidth, 
-                _puzzle.Height * CellSize);
+            cr.Rectangle(_puzzleStart.X, PuzzleMargin, Puzzle.Width * CellSize,
+                Puzzle.Height * CellSize + _cluesHeight);
+            cr.Rectangle(PuzzleMargin, _puzzleStart.Y, Puzzle.Width * CellSize + _cluesWidth, 
+                Puzzle.Height * CellSize);
             cr.Fill();
         }
 
@@ -226,16 +226,16 @@ namespace GCross
         private void DrawHighlight(Context cr)
         {
             cr.SetSourceRGBA(0, 0.83, 0.98, .2);
-            if (_selectedIndex.X >= 0 && _selectedIndex.X < _puzzle.Width)
+            if (_selectedIndex.X >= 0 && _selectedIndex.X < Puzzle.Width)
             {
                 int x = _selectedIndex.X * CellSize + _puzzleStart.X;
-                cr.Rectangle(x, PuzzleMargin, CellSize, _puzzle.Height * CellSize + _cluesHeight);
+                cr.Rectangle(x, PuzzleMargin, CellSize, Puzzle.Height * CellSize + _cluesHeight);
             }
             
-            if (_selectedIndex.Y >= 0 && _selectedIndex.Y < _puzzle.Height)
+            if (_selectedIndex.Y >= 0 && _selectedIndex.Y < Puzzle.Height)
             {
                 int y = _selectedIndex.Y * CellSize + _puzzleStart.Y;
-                cr.Rectangle(PuzzleMargin, y, _puzzle.Width * CellSize + _cluesWidth, CellSize);
+                cr.Rectangle(PuzzleMargin, y, Puzzle.Width * CellSize + _cluesWidth, CellSize);
             }
             
             cr.Fill();
@@ -249,16 +249,16 @@ namespace GCross
         {
             cr.SetSourceRGBA(0, 0.83, 0.98, 1);
             cr.LineWidth = BorderLineWidth;
-            if (_selectedIndex.X >= 0 && _selectedIndex.X < _puzzle.Width)
+            if (_selectedIndex.X >= 0 && _selectedIndex.X < Puzzle.Width)
             {
                 int x = _selectedIndex.X * CellSize + _puzzleStart.X;
-                cr.Rectangle(x, _puzzleStart.Y, CellSize, _puzzle.Height * CellSize);
+                cr.Rectangle(x, _puzzleStart.Y, CellSize, Puzzle.Height * CellSize);
             }
             
-            if (_selectedIndex.Y >= 0 && _selectedIndex.Y < _puzzle.Height)
+            if (_selectedIndex.Y >= 0 && _selectedIndex.Y < Puzzle.Height)
             {
                 int y = _selectedIndex.Y * CellSize + _puzzleStart.Y;
-                cr.Rectangle(_puzzleStart.X, y, _puzzle.Width * CellSize, CellSize);
+                cr.Rectangle(_puzzleStart.X, y, Puzzle.Width * CellSize, CellSize);
             }
 
             cr.Stroke();
@@ -274,45 +274,57 @@ namespace GCross
             cr.SetFontSize(20);
             
             // Column clues
-            for (int cx = 0; cx < _puzzle.Width; cx++)
+            for (int cx = 0; cx < Puzzle.Width; cx++)
             {
-                int[] clues = _puzzle.ColumnClues[cx];
-                ClueStatus[] cluesStatus = _puzzle.ColumnCluesStatus[cx];
+                int[] clues = Puzzle.ColumnClues[cx];
+                ClueStatus[] cluesStatus = Puzzle.ColumnCluesStatus[cx];
                 int x = _puzzleStart.X + cx * CellSize;
                 for (int i = 0; i < clues.Length; i++)
                 {
+                    if (cluesStatus[clues.Length - i - 1] == ClueStatus.Unmarked)
+                    {
+                        cr.SelectFontFace("Arial", FontSlant.Normal, FontWeight.Bold);
+                        cr.SetSourceRGBA(0, 0, 0, 1);
+                    }
+                    else
+                    {
+                        cr.SelectFontFace("Arial", FontSlant.Normal, FontWeight.Normal);
+                        cr.SetSourceRGBA(0.5, 0.5, 0.5, 1);
+                    }
+                    
                     string clue = clues[clues.Length - i - 1].ToString();
                     TextExtents te = cr.TextExtents(clue);
                     cr.MoveTo(x + (CellSize - te.Width) / 2.0 - te.XBearing,
                         _puzzleStart.Y - (CellSize * i) - (CellSize - te.Height) / 2.0);
 
-                    if (cluesStatus[clues.Length - i - 1] == ClueStatus.Unmarked)
-                        cr.SetSourceRGBA(0, 0, 0, 1);
-                    else
-                        cr.SetSourceRGBA(0.5, 0.5, 0.5, 1);
-                    
                     cr.ShowText(clue);
                 }
             }
             
             // Row clues
-            for (int cy = 0; cy < _puzzle.Height; cy++)
+            for (int cy = 0; cy < Puzzle.Height; cy++)
             {
-                int[] clues = _puzzle.RowClues[cy];
-                ClueStatus[] cluesStatus = _puzzle.RowCluesStatus[cy];
+                int[] clues = Puzzle.RowClues[cy];
+                ClueStatus[] cluesStatus = Puzzle.RowCluesStatus[cy];
                 int y = _puzzleStart.Y + (cy + 1) * CellSize;
                 for (int i = 0; i < clues.Length; i++)
                 {
+                    if (cluesStatus[clues.Length - i - 1] == ClueStatus.Unmarked)
+                    {
+                        cr.SelectFontFace("Arial", FontSlant.Normal, FontWeight.Bold);
+                        cr.SetSourceRGBA(0, 0, 0, 1);
+                    }
+                    else
+                    {
+                        cr.SelectFontFace("Arial", FontSlant.Normal, FontWeight.Normal);
+                        cr.SetSourceRGBA(0.5, 0.5, 0.5, 1);
+                    }
+                    
                     string clue = clues[clues.Length - i - 1].ToString();
                     TextExtents te = cr.TextExtents(clue);
                     cr.MoveTo(_puzzleStart.X - (CellSize * (i + 1)) + (CellSize - te.Width) / 2.0 - te.XBearing,
                         y - (CellSize - te.Height) / 2.0);
-                    
-                    if (cluesStatus[clues.Length - i - 1] == ClueStatus.Unmarked)
-                        cr.SetSourceRGBA(0, 0, 0, 1);
-                    else
-                        cr.SetSourceRGBA(0.5, 0.5, 0.5, 1);
-                    
+
                     cr.ShowText(clue);
                 }
             }
@@ -330,45 +342,45 @@ namespace GCross
             cr.LineWidth = BorderLineWidth;
             
             cr.MoveTo(_puzzleStart.X, PuzzleMargin);
-            cr.LineTo(_puzzleStart.X, _puzzleStart.Y + _puzzle.Height * CellSize);
+            cr.LineTo(_puzzleStart.X, _puzzleStart.Y + Puzzle.Height * CellSize);
             
-            cr.MoveTo(_puzzleStart.X + _puzzle.Width * CellSize, PuzzleMargin);
-            cr.LineTo(_puzzleStart.X + _puzzle.Width * CellSize, _puzzleStart.Y + _puzzle.Height * CellSize);
+            cr.MoveTo(_puzzleStart.X + Puzzle.Width * CellSize, PuzzleMargin);
+            cr.LineTo(_puzzleStart.X + Puzzle.Width * CellSize, _puzzleStart.Y + Puzzle.Height * CellSize);
 
             cr.MoveTo(PuzzleMargin, _puzzleStart.Y);
-            cr.LineTo(_puzzleStart.X + _puzzle.Width * CellSize, _puzzleStart.Y);
+            cr.LineTo(_puzzleStart.X + Puzzle.Width * CellSize, _puzzleStart.Y);
 
-            cr.MoveTo(PuzzleMargin, _puzzleStart.Y + _puzzle.Height * CellSize);
-            cr.LineTo(_puzzleStart.X + _puzzle.Width * CellSize, _puzzleStart.Y + _puzzle.Height * CellSize);
+            cr.MoveTo(PuzzleMargin, _puzzleStart.Y + Puzzle.Height * CellSize);
+            cr.LineTo(_puzzleStart.X + Puzzle.Width * CellSize, _puzzleStart.Y + Puzzle.Height * CellSize);
             
             cr.Stroke();
             
             // Major grid (every five cells)
             cr.LineWidth = MajorGridLineWidth;
-            for (int x = 1; x <= _puzzle.Width / 5; x++)
+            for (int x = 1; x <= Puzzle.Width / 5; x++)
             {
                 cr.MoveTo(x * 5 * CellSize + _puzzleStart.X, PuzzleMargin);
-                cr.LineTo(x * 5 * CellSize + _puzzleStart.X, _puzzle.Height * CellSize + _puzzleStart.Y);
+                cr.LineTo(x * 5 * CellSize + _puzzleStart.X, Puzzle.Height * CellSize + _puzzleStart.Y);
             }
-            for (int y = 1; y <= _puzzle.Height / 5; y++)
+            for (int y = 1; y <= Puzzle.Height / 5; y++)
             {
                 cr.MoveTo(PuzzleMargin, y * 5 * CellSize + _puzzleStart.Y);
-                cr.LineTo(_puzzle.Width * CellSize + _puzzleStart.X, y * 5 * CellSize + _puzzleStart.Y);
+                cr.LineTo(Puzzle.Width * CellSize + _puzzleStart.X, y * 5 * CellSize + _puzzleStart.Y);
             }
             
             cr.Stroke();
                 
             // Minor grid (every cell)
             cr.LineWidth = MinorGridLineWidth;
-            for (int x = 1; x < _puzzle.Width; x++)
+            for (int x = 1; x < Puzzle.Width; x++)
             {
                 cr.MoveTo(x * CellSize + _puzzleStart.X, PuzzleMargin);
-                cr.LineTo(x * CellSize + _puzzleStart.X, _puzzle.Height * CellSize + _puzzleStart.Y);
+                cr.LineTo(x * CellSize + _puzzleStart.X, Puzzle.Height * CellSize + _puzzleStart.Y);
             }
-            for (int y = 1; y < _puzzle.Height; y++)
+            for (int y = 1; y < Puzzle.Height; y++)
             {
                 cr.MoveTo(PuzzleMargin, y * CellSize + _puzzleStart.Y);
-                cr.LineTo(_puzzle.Width * CellSize + _puzzleStart.X, y * CellSize + _puzzleStart.Y);
+                cr.LineTo(Puzzle.Width * CellSize + _puzzleStart.X, y * CellSize + _puzzleStart.Y);
             }
             
             cr.Stroke();
@@ -383,19 +395,19 @@ namespace GCross
             cr.SetSourceRGBA(0, 0, 0, 1);
             cr.LineWidth = 2;
             
-            for (int x = 0; x < _puzzle.Width; x++)
+            for (int x = 0; x < Puzzle.Width; x++)
             {
-                for (int y = 0; y < _puzzle.Height; y++)
+                for (int y = 0; y < Puzzle.Height; y++)
                 {
                     int cellX = _puzzleStart.X + x * CellSize;
                     int cellY = _puzzleStart.Y + y * CellSize;
                     
-                    if (_puzzle.Grid[x, y] == CellStatus.Fill)
+                    if (Puzzle.Grid[x, y] == CellStatus.Fill)
                     {
                         cr.Rectangle(cellX, cellY, CellSize, CellSize);
                         cr.Fill();
                     }
-                    else if (_puzzle.Grid[x, y] == CellStatus.Cross)
+                    else if (Puzzle.Grid[x, y] == CellStatus.Cross)
                     {
                         // Clip the cross in this square so it doesn't extend to other cells
                         cr.Rectangle(cellX, cellY, CellSize, CellSize);
@@ -431,7 +443,7 @@ namespace GCross
             x = x / _drawScale / CellSize;
             y = y / _drawScale / CellSize;
 
-            if (x >= _puzzle.Width || y >= _puzzle.Height ||
+            if (x >= Puzzle.Width || y >= Puzzle.Height ||
                 x < (double) -_cluesWidth / CellSize || y < (double) -_cluesHeight / CellSize) 
                 return (-1, -1);
                     
@@ -450,7 +462,7 @@ namespace GCross
             
             CellStatus cellStatus = CellStatus.None;
             if (cx >= 0 && cy >= 0)
-                cellStatus = _puzzle.Grid[cx, cy];
+                cellStatus = Puzzle.Grid[cx, cy];
             
             if (_buttonJustPressed)
             {
@@ -462,28 +474,28 @@ namespace GCross
                     if (cx < 0)
                     {
                         // Row clue clicked
-                        int clueIndex = cx + _puzzle.RowClues[cy].Length;
+                        int clueIndex = cx + Puzzle.RowClues[cy].Length;
                         if (clueIndex >= 0)
                         {
-                            _puzzle.RowCluesStatus[cy][clueIndex] = _puzzle.RowCluesStatus[cy][clueIndex] switch
+                            Puzzle.RowCluesStatus[cy][clueIndex] = Puzzle.RowCluesStatus[cy][clueIndex] switch
                             {
                                 ClueStatus.Unmarked => ClueStatus.Marked,
                                 ClueStatus.Marked => ClueStatus.Unmarked,
-                                _ => _puzzle.RowCluesStatus[cy][clueIndex]
+                                _ => Puzzle.RowCluesStatus[cy][clueIndex]
                             };
                         }
                     }
                     else if (cy < 0)
                     {
                         // Column clue clicked
-                        int clueIndex = cy + _puzzle.ColumnClues[cx].Length;
+                        int clueIndex = cy + Puzzle.ColumnClues[cx].Length;
                         if (clueIndex >= 0)
                         {
-                            _puzzle.ColumnCluesStatus[cx][clueIndex] = _puzzle.ColumnCluesStatus[cx][clueIndex] switch
+                            Puzzle.ColumnCluesStatus[cx][clueIndex] = Puzzle.ColumnCluesStatus[cx][clueIndex] switch
                             {
                                 ClueStatus.Unmarked => ClueStatus.Marked,
                                 ClueStatus.Marked => ClueStatus.Unmarked,
-                                _ => _puzzle.ColumnCluesStatus[cx][clueIndex]
+                                _ => Puzzle.ColumnCluesStatus[cx][clueIndex]
                             };
                         }
                     }
@@ -503,7 +515,7 @@ namespace GCross
             {
                 if (cellStatus != _clickAction)
                 {
-                    _puzzle.Grid[cx, cy] = _clickAction;
+                    Puzzle.Grid[cx, cy] = _clickAction;
                 }
             }
         }
